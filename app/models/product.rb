2834cfeef8d64cf6a2e7507e3f9856e0dae3ab86
Koki Kaminoura <amino.t02y@gmail.com>
2017-02-26 10:39:05 +0900

    define assosiation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  has_many :purchase_logs
+  has_many :users, through: :purchase_logs
+
+  belongs_to :category, foreign_key: 'middle_category_name', primary_key: :middle_category_name
+
 end
--- app/models/purchase_log.rb
+++ app/models/purchase_log.rb
@@ -1,2 +1,4 @@
 class PurchaseLog < ApplicationRecord
+  belongs_to :user
+  belongs_to :product
 end
